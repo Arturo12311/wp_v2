c709483643515b67c5564acd5a20228c96d3f2af
Arturo12311 <121392873+Arturo12311@users.noreply.github.com>
2024-06-25 16:22:03 -0400

    Create structures.exs

---
+++ structures.exs
@@ -0,0 +1,24 @@
+[
+  {"InfoAuthenticate",
+  [
+    %{name: "local_area_network_address", type: {:string, :dynamic}},
+    %{name: "market", type: {:uint, 4}},
+    %{name: "authentication_kind", type: {:uint, 4}},
+    %{name: "os_kind", type: {:uint, 4}},
+    %{name: "os_version", type: {:string, :dynamic}},
+    %{name: "device_model", type: {:string, :dynamic}},
+    %{name: "adid", type: {:string, :dynamic}},
+    %{name: "idfv", type: {:string, :dynamic}},
+    %{name: "country_code", type: {:string, :dynamic}},
+    %{name: "language", type: {:string, :dynamic}},
+    %{name: "app_version", type: {:string, :dynamic}},
+    %{name: "os_type", type: {:uint, 4}},
+    %{name: "os_name", type: {:string, :dynamic}},
+    %{name: "npsn", type: {:string, :dynamic}},
+    %{name: "np_token", type: {:string, :dynamic}},
+    %{name: "ngsm_token", type: {:string, :dynamic}},
+    %{name: "npa_code", type: {:string, :dynamic}},
+    %{name: "nexon_sn", type: {:uint, 4}},
+    %{name: "terminator", type: {:uint, 4}}
+  ]}
+ ]
